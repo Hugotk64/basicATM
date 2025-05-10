a65470e9537a96f936c86ac37f16c7d914e39d16
Hugo Castillo <112522025+Hugotk64@users.noreply.github.com>
2025-05-10 05:18:37 +0000

    function to generate account

--- src/account.c
+++ src/account.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+#include "../headers/account.h"
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int generateAccountNumber(Account *account, int size) {
+    int accountNumber;
+    
+    do
+    {
+        // Generate a random account number between 100000 and 999999
+        accountNumber = 100000 + rand() % 900000;
+
+        // Check if the account number already exists
+        for (int i = 0; i < size; i++) {
+            if (account[i].accountNumber == accountNumber) {
+                accountNumber = 0;
+                break;
+            }
+        }
+
+    } while (accountNumber == 0);
+    return accountNumber;
 }
