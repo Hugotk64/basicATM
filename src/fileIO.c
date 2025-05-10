d9f9794cc6533ed59107e121e00bbe11a361e373
Hugo Castillo <112522025+Hugotk64@users.noreply.github.com>
2025-05-10 05:36:28 +0000

    loadAccounts

--- src/fileIO.c
+++ src/fileIO.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "../headers/account.h"
+
+Account* loadAccounts(int *size) {
+    FILE *file = fopen("../data/accounts.txt", "r");
+    if (!file) {
+        *size = 0;
+        return NULL;
+    }
+
+    Account *accounts = NULL;
+    int count = 0;
+    char line[256];
+
+    while (fgets(line, sizeof(line), file)) {
+        Account acc;
+        
+        if (sscanf(line, "%d %d %49[^,] %f", 
+            &acc.accountNumber, 
+            &acc.pin, 
+            acc.name, 
+            &acc.balance) == 4) {
+            
+            accounts = realloc(accounts, (count + 1) * sizeof(Account));
+            accounts[count] = acc;
+            count++;
+        }
+    } 
+
+    // Close the file
+    fclose(file);
+    *size = count;
+    return accounts;
+}
 
 void saveAccounts(Account *account, int size) {
     
